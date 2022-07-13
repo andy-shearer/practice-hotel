765754d4c6498869c5ddaa5b2111a96938f3daaa
Andy Shearer <andyshearer.95@googlemail.com>
2022-07-13 18:30:09 +0100

    Add test to search for multiple bookings

--- src/test/java/BookingSystemTest.java
+++ src/test/java/BookingSystemTest.java
@@ -42,8 +42,8 @@ public class BookingSystemTest {
     }
 
     @Test
-    public void testFindGuestBookings() {
-        // Set up a booking into the system for us to lookup
+    public void testFindSingleGuestBooking() {
+        // Set up a booking in the system for us to lookup
         boolean booked = bookingSystem.createBooking("Andy Shearer", DEFAULT_ARRIVAL, DEFAULT_CHECKOUT);
         assertTrue("Expected booking to be successful", booked);
 
@@ -55,5 +55,30 @@ public class BookingSystemTest {
         assertEquals("Expected booking name to match provided info", DEFAULT_NAME, guestBooking.getGuestName());
         assertEquals("Expected booking arrival to match provided info", DEFAULT_ARRIVAL, guestBooking.getArrival());
         assertEquals("Expected booking checkout to match provided info", DEFAULT_CHECKOUT, guestBooking.getCheckOut());
+    }
+
+    @Test
+    public void testFindMultipleGuestBookings() {
+        Date secondArrival = Date.from(DEFAULT_ARRIVAL.toInstant().plus(7, ChronoUnit.DAYS));
+        Date secondCheckout = Date.from(secondArrival.toInstant().plus(3, ChronoUnit.DAYS));
+
+        // Set up a booking in the system for us to lookup
+        boolean booked = bookingSystem.createBooking("Andy Shearer", DEFAULT_ARRIVAL, DEFAULT_CHECKOUT);
+        booked = booked && bookingSystem.createBooking("Andy Shearer", secondArrival, secondCheckout);
+        assertTrue("Expected bookings to be successful", booked);
+
+        List<Booking> roomBookings = bookingSystem.bookingsForGuest(DEFAULT_NAME);
+        assertNotNull("Expected bookings to be found", roomBookings);
+        assertEquals("Expected 2 booking to be returned", 2, roomBookings.size());
+
+        Booking guestBooking = roomBookings.get(0);
+        assertEquals("Expected booking name to match provided info", DEFAULT_NAME, guestBooking.getGuestName());
+        assertEquals("Expected booking arrival to match provided info", DEFAULT_ARRIVAL, guestBooking.getArrival());
+        assertEquals("Expected booking checkout to match provided info", DEFAULT_CHECKOUT, guestBooking.getCheckOut());
+
+        guestBooking = roomBookings.get(1);
+        assertEquals("Expected booking name to match provided info", DEFAULT_NAME, guestBooking.getGuestName());
+        assertEquals("Expected booking arrival to match provided info", secondArrival, guestBooking.getArrival());
+        assertEquals("Expected booking checkout to match provided info", secondCheckout, guestBooking.getCheckOut());
     }
 }
